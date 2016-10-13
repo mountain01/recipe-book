8088f3ef2191bddf630a74e17cac30ca155c2419
Matt Edwards <edwardsjm@mtc.byu.edu>
2016-10-12 18:10:53 -0600

    do some clean up

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe';
 import { RecipeService } from '../recipe.service';
@@ -13,9 +13,5 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onSelected(recipe: Recipe) {
-    //this.recipeSelected.emit(recipe);
   }
 }
