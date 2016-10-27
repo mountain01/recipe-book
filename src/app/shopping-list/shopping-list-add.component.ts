dbefbe0a5a18b8ed8e2e348e1383d2f454ab4a0e
Matt Edwards <edwardsjm@mtc.byu.edu>
2016-10-27 10:53:42 -0600

    add form stuff to project for editing shopping list and recipes

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,14 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges {
+  isAdd: boolean = true;
+  @Input() item: Ingredient;
+  @Output() cleared = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private sls: ShoppingListService) {
   }
 
+  ngOnChanges(changes) {
+    if (changes.item.currentValue === null) {
+      this.isAdd = true;
+      this.item = { name: null, amount: null };
+    } else {
+      this.isAdd = false;
+    }
+  }
+
+  onSubmit(ingredient: Ingredient) {
+    if (this.isAdd) {
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.sls.addItem(this.item);
+      this.onClear();
+    } else {
+      this.sls.editItem(this.item, new Ingredient(ingredient.name, ingredient.amount));
+    }
+  }
+
+  onDelete() {
+    this.sls.deleteItem(this.item);
+    this.onClear();
+  }
+
+  onClear() {
+    this.isAdd = true;
+    this.cleared.emit(null);
+  }
 }
